301cf9a92c689e7042a78217988d7ba1d955b16b
Jessica Stern <jlstern3@gmail.com>
2021-10-17 19:25:04 -0400

    Roughly outlined User & Program models

--- jacks_website_proj/company_app/models.py
+++ jacks_website_proj/company_app/models.py
@@ -1,4 +1,4 @@
-# from django.db import models
+from django.db import models
 # import re
 
 # class User(models.Model):
@@ -8,5 +8,13 @@
 #     location = models.TextField(null = True)
 #     email = models.TextField()
 #     password = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+
+# class Program(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.CharField(max_length=255)
+#     price = models.IntegerField
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now=True)
